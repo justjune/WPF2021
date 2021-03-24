3422c7578b9c4b9f5c8918b293631456cabf8559
Fffacelesss <ksenija_demina@rambler.ru>
2021-03-24 20:27:20 +0500

    Исправила главы 1-2, добавила главы 3-6

---
+++ Chapter_05_0101 StackThirtyButtons/StackThirtyButtons.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.StackThirtyButtons
+{
+    class StackThirtyButtons : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new StackThirtyButtons());
+        }
+        public StackThirtyButtons()
+        {
+            Title = "Stack Thirty Buttons";
+            SizeToContent = SizeToContent.WidthAndHeight; // задаем размер окна по размеру контента в нем
+            ResizeMode = ResizeMode.CanMinimize; // убираем кнопку которая разворачивает окно по размеру дисплея
+            AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick)); //при нажатии на кнопку вызываем ф-ию ButtonOnClick
+
+            StackPanel stackMain = new StackPanel(); // объединяем кнопки в группы, задаем им ориентацию и отступы вокруг
+            stackMain.Orientation = Orientation.Horizontal;
+            stackMain.Margin = new Thickness(5);
+            Content = stackMain;
+
+            for (int i = 0; i < 3; i++)    // объединяем кнопки в группы по 3
+            {
+                StackPanel stackChild = new StackPanel();
+                stackMain.Children.Add(stackChild);
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Button btn = new Button(); // создаем кнопки, задаем отступ и надпись в них
+                    btn.Content = "Button No. " + (10 * i + j + 1);
+                    btn.Margin = new Thickness(5);
+                    stackChild.Children.Add(btn);
+                }
+            }
+        }
+        void ButtonOnClick(object sender, RoutedEventArgs args) // при нажатии на кнопку выводим в новом окне текст
+        {
+            MessageBox.Show("You clicked the button labeled " +
+                            (args.Source as Button).Content);
+        }
+    }
+}
---
+++ Chapter_06_0133 SplitTheClient/SplitTheClient.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+class SplitTheClient : Window
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application app = new Application();
+        app.Run(new SplitTheClient());
+    }
+    public SplitTheClient()
+    {
+        Title = "Split the Client";
+
+        Grid grid1 = new Grid(); // область переменного размера 
+        grid1.ColumnDefinitions.Add(new ColumnDefinition());
+        grid1.ColumnDefinitions.Add(new ColumnDefinition());
+        grid1.ColumnDefinitions.Add(new ColumnDefinition());
+        grid1.ColumnDefinitions[1].Width = GridLength.Auto;
+        Content = grid1;
+
+        Button btn = new Button(); // кнопка слева
+        btn.Content = "Button No. 1";
+        grid1.Children.Add(btn);
+        Grid.SetRow(btn, 0);
+        Grid.SetColumn(btn, 0);
+
+        GridSplitter split = new GridSplitter(); // вертикальное разделение
+        split.ShowsPreview = true;
+        split.HorizontalAlignment = HorizontalAlignment.Center;
+        split.VerticalAlignment = VerticalAlignment.Stretch;
+        split.Width = 6;
+        grid1.Children.Add(split);
+        Grid.SetRow(split, 0);
+        Grid.SetColumn(split, 1);
+
+        Grid grid2 = new Grid(); // grid с горизонтальным разделением
+        grid2.RowDefinitions.Add(new RowDefinition());
+        grid2.RowDefinitions.Add(new RowDefinition());
+        grid2.RowDefinitions.Add(new RowDefinition());
+        grid2.RowDefinitions[1].Height = GridLength.Auto;
+        grid1.Children.Add(grid2);
+        Grid.SetRow(grid2, 0);
+        Grid.SetColumn(grid2, 2);
+
+        btn = new Button(); // кнопка над горизонтальным разделением
+        btn.Content = "Button No. 2";
+        grid2.Children.Add(btn);
+        Grid.SetRow(btn, 0);
+        Grid.SetColumn(btn, 0);
+
+        split = new GridSplitter(); // горизонтальное разделение
+        split.ShowsPreview = true;
+        split.HorizontalAlignment = HorizontalAlignment.Stretch;
+        split.VerticalAlignment = VerticalAlignment.Center;
+        split.Height = 6;
+        grid2.Children.Add(split);
+        Grid.SetRow(split, 1);
+        Grid.SetColumn(split, 0);
+
+        btn = new Button(); // кнопка под горизонтальным разделением
+        btn.Content = "Button No. 3";
+        grid2.Children.Add(btn);
+        Grid.SetRow(btn, 2);
+        Grid.SetColumn(btn, 0);
+    }
+}
