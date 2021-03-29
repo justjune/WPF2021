822c99f96b48eea3aca97cb0c27ab54126289eae
yunostivkus <cameramk@mail.ru>
2021-03-29 19:47:10 +0500

    Пометил

--- Chapter_06 127 SpanTheCells/SpanTheCells.cs
+++ Chapter_06 127 SpanTheCells/SpanTheCells.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/////////////////////////
+//   Казаринов Матвей  //
+/////////////////////////
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
