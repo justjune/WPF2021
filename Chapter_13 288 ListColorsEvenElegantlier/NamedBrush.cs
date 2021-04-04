9c8e545f0038dc80f729e8b95f8233d17f330d03
Jarosloov <jarosloov@yandex.ru>
2021-04-04 12:42:43 +0500

    Добавление двух проектов

---
+++ Chapter_13 288 ListColorsEvenElegantlier/NamedBrush.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Media;
+
+namespace Chapter_13_288_ListColorsEvenElegantlier
+{
+    class NamedBrush
+    {
+        static NamedBrush[] nbrushes; 
+        Brush brush; 
+        string str;   
+        // Static constructor.  
+        static NamedBrush()     
+        {         
+            PropertyInfo[] props = typeof(Brushes) .GetProperties();   
+            nbrushes = new NamedBrush[props.Length];         
+            for (int i = 0; i < props.Length; i++)
+            {
+                nbrushes[i] = new NamedBrush(props[i].Name,
+                    (Brush)props[i].GetValue(null, null));
+            }          
+                
+        }        
+        // Private constructor.   
+        private NamedBrush(string str, Brush brush)   
+        {           
+            this.str = str;      
+            this.brush = brush;    
+        }        
+        
+        // Static read-only property. 
+        public static NamedBrush[] All   
+        {           
+            get { return nbrushes; }  
+        }        
+        // Read-only properties.   
+        public Brush Brush      
+        {             
+            get { return brush; }   
+        }         
+        public string Name   
+        {            
+            get      
+            {        
+                string strSpaced = str[0].ToString();       
+                for (int i = 1; i < str.Length; i++)     
+                    strSpaced += (char.IsUpper (str[i]) ? " " : "") +      
+                        str[i] .ToString();               
+                return strSpaced;             
+            }         
+        }        
+        // Override of ToString method.  
+        public override string ToString()    
+        {            
+            return str;  
+        }  
+    }
+}
