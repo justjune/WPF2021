0d516d2b73f6f403e791f5fa21f56c349baee4d8
nvbtds@gmail.com <nvbtds@gmail.com>
2021-04-16 22:43:53 +0500

    Chapter_13 271 and Chapter_13 292

---
+++ Chapter_13 271 ListColorValues/ListColorValues.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Reflection;
+
+namespace Chapter_13_271_ListColorValues
+{
+    class ListColorValues : Window
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run(new ListColorValues());
+        }
+
+        public ListColorValues()
+        {
+            Title = "List Color Values";
+            
+            // Создание объекта ListBox как содержимого окна
+            ListBox lstbox = new ListBox();
+            lstbox.Width = 150;
+            lstbox.Height = 150;
+            lstbox.SelectionChanged += ListBoxOnSelectionChanged;  // Назначаем обработчик события SelectionChanged
+            Content = lstbox;
+
+            // Заполнение ListBox объектами Color
+            PropertyInfo[] props = typeof(Colors).GetProperties();  // Получаем информацию о свойствах класса Color
+
+            foreach (PropertyInfo prop in props)
+                lstbox.Items.Add( // Добавляем новый элемент в ListBox
+                    prop.GetValue(null, null));  // Получаем объект (цвет) из Colors
+        }
+
+        void ListBoxOnSelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            ListBox lstbox = sender as ListBox;
+
+            if (lstbox.SelectedIndex != -1)  // Проверяем, что есть выделение
+            {
+                Color clr = (Color)lstbox.SelectedItem;  // Получаем выбранный цвет
+                Background = new SolidColorBrush(clr);  // Закрашиваем фон этим цветом
+            }
+        }
+    }
+}
---
+++ Chapter_13 292 SelectColorFromGrid/ColorGridBox.cs
@@ -0,0 +1,57 @@
+﻿//--------------------------------------------- 
+// ColorGridBox.cs (c) 2006 by Charles Petzold 
+//---------------------------------------------
+using System; using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media; 
+using System.Windows.Shapes; 
+namespace Petzold.SelectColorFromGrid 
+{     
+    class ColorGridBox : ListBox     
+    { //Отображаемые цвета
+        string[] strColors = 
+        {
+            "Black", "Brown", "DarkGreen",  "MidnightBlue",                 
+            "Navy", "DarkBlue", "Indigo",  "DimGray",             
+            "DarkRed", "OrangeRed", "Olive", "Green",                 
+            "Teal", "Blue", "SlateGray", "Gray",             
+            "Red", "Orange", "YellowGreen",  "SeaGreen",                 
+            "Aqua", "LightBlue", "Violet",  "DarkGray",             
+            "Pink", "Gold", "Yellow", "Lime",                 
+            "Turquoise", "SkyBlue", "Plum",  "LightGray",             
+            "LightPink", "Tan", "LightYellow",  "LightGreen",                 
+            "LightCyan", "LightSkyBlue",  "Lavender", "White"
+        };         
+        
+        public ColorGridBox()         
+        { 
+            //Определение шаблона и его включение в ListBox
+            FrameworkElementFactory factoryUnigrid = new  FrameworkElementFactory(typeof(UniformGrid));             
+            factoryUnigrid.SetValue(UniformGrid .ColumnsProperty, 8);             
+            ItemsPanel = new ItemsPanelTemplate (factoryUnigrid); 
+            
+            //заполнение списка         
+            foreach (string strColor in strColors)             
+            { 
+                //Создание объекта Rectangle и включение его в ListBox               
+                Rectangle rect = new Rectangle();                 
+                rect.Width = 12;                 
+                rect.Height = 12;                 
+                rect.Margin = new Thickness(4);                 
+                rect.Fill = (Brush)                     
+                typeof(Brushes).GetProperty (strColor).GetValue(null, null);                 
+                Items.Add(rect); 
+                
+                //Создание объекта ToolTip для Rectangle
+                ToolTip tip = new ToolTip();                 
+                tip.Content = strColor;                 
+                rect.ToolTip = tip;             
+            }
+            //В качестве выбранного значения выбирается свойствo fill объекта Rectangle         
+            SelectedValuePath = "Fill";         
+        }     
+    } 
+} 
