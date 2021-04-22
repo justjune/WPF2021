24192f5d73b7404c50b8b9412b5a1aeddcdb568a
Jorik <Trubnikov@unhappy.com>
2021-04-22 14:16:19 +0500

    CustomClass

---
+++ Chapter_19 494 UseCustomClass/UseCustomClass.cs
@@ -0,0 +1,28 @@
+﻿using Chapter_13_295_SelectColorFromWheel;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+namespace Chapter_19_494_UseCustomClass
+{
+    public partial class UseCustomClass : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new UseCustomClass());
+        }
+        public UseCustomClass()
+        {
+            InitializeComponent();
+        }
+
+        void ColorGridBoxOnSelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            ColorWheel clrbox = args.Source as ColorWheel;
+            Background = (Brush)clrbox.SelectedValue;
+        }
+    }
+}
