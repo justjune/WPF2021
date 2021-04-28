eb200a4d9aa3589184467ae4218bb548af2ceceb
Jarosloov <jarosloov@yandex.ru>
2021-04-28 23:11:12 +0500

    Chapter_19_491_CompileXamlWindow

--- Chapter_19 484 CompileXamlWindow/CompileXamlWindow.cs
+++ Chapter_19 484 CompileXamlWindow/CompileXamlWindow.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Chapter_19_484_CompileXamlWindow
 {
-    class CompileXamlWindow
+    public partial class CompileXamlWindow : Window
     {
-    }
+        [STAThread]
+        public static void Main() 
+        { 
+            Application app = new Application(); 
+            pp.Run(new CompileXamlWindow());
+        }
+        public CompileXamlWindow()
+        {            
+            // Required method call to hook up  event handlers and        
+            // initialize fields.           
+            InitializeComponent();          
+            // Fill up the ListBox with brush names.    
+            foreach (PropertyInfo prop in typeof (Brushes).GetProperties())  
+                lstbox.Items.Add(prop.Name);         
+        }        
+        // Button event handler just displays  MessageBox.   
+        void ButtonOnClick(object sender,  RoutedEventArgs args)  
+        {          
+            Button btn = sender as Button;  
+            MessageBox.Show("The button labled '"  + btn.Content +     
+                "' has been clicked.");         } 
+        // ListBox event handler changes Fill  property of Ellipse.   
+        void ListBoxOnSelection(object sender,  SelectionChangedEventArgs args) 
+        {             ListBox lstbox = sender as ListBox;       
+            string strItem = lstbox.SelectedItem  as string;    
+            PropertyInfo prop = typeof(Brushes) .GetProperty(strItem);   
+            elips.Fill = (Brush)prop.GetValue(null , null);     
+        } 
 }
