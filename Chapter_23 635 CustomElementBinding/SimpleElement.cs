32343e56fb933733fe5c0ee35f6fc76d984728f4
Andrey Rechkalov <61356488+BizonAR@users.noreply.github.com>
2021-04-29 23:55:43 +0500

    Andrey Rechkalov

---
+++ Chapter_23 635 CustomElementBinding/SimpleElement.cs
@@ -0,0 +1,50 @@
+﻿//----------------------------------------------
+// SimpleElement.cs (c) 2006 by Charles Petzold
+//----------------------------------------------
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Petzold.CustomElementBinding
+{
+    class SimpleElement : FrameworkElement
+    {
+        // Определение DependencyProperty.
+        public static DependencyProperty NumberProperty;
+
+        // Создать DependencyProperty в статическом конструкторе.
+        static SimpleElement()
+        {
+            NumberProperty =
+                DependencyProperty.Register("Number", typeof(double),
+                                            typeof(SimpleElement),
+                    new FrameworkPropertyMetadata(0.0,
+                            FrameworkPropertyMetadataOptions.AffectsRender));
+        }
+
+        // DependencyProperty оформляется в виде свойства CLR.
+        public double Number
+        {
+            set { SetValue(NumberProperty, value); }
+            get { return (double)GetValue(NumberProperty); }
+        }
+
+        // Жёсткое кодирование размера для MeasureOverride.
+        protected override Size MeasureOverride(Size sizeAvailable)
+        {
+            return new Size(200, 50);
+        }
+
+        // Метод OnRender выводит значения свойства Number.
+        protected override void OnRender(DrawingContext dc)
+        {
+            dc.DrawText(
+                new FormattedText(Number.ToString(),
+                        CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
+                        new Typeface("Times New Roman"), 12,
+                        SystemColors.WindowTextBrush),
+                new Point(0, 0));
+        }
+    }
+}
