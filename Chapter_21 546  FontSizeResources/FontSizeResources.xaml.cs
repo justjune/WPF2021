5a2e44d89af730da96d80d86209e892fc4806111
Jarosloov <jarosloov@yandex.ru>
2021-05-26 22:59:08 +0500

    [ Группа 3 ] [Глава 21] 1. FontSizeResources.xaml ResourceLookupDemo.xaml [Глава 31] 6. AnimatedDrawingBrush.xaml 7. ConvertedBitmapChain.xaml 8. FishBrush.xaml

---
+++ Chapter_21 546  FontSizeResources/FontSizeResources.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter_21_546__FontSizeResources
+{
+    /// <summary>
+    /// Логика взаимодействия для FontSizeResources.xaml
+    /// </summary>
+    public partial class FontSizeResources : UserControl
+    {
+        public FontSizeResources()
+        {
+            InitializeComponent();
+        }
+    }
+}
